6092c1aeb99724db5941ee3c8404541dcc07fb19
hassy <hassy@owl0.logos.ic.i.u-tokyo.ac.jp>
2017-04-26 16:28:35 +0900

    Modified the test1 function and README

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # cpp-neural-exercise
-Basic C++ programming exercise by implementing simple neural networks from scratch
+This project aims at providing an opportunity to do basic C++ programming exercise by implementing simple neural networks from scratch.<br>
+The exercise does NOT include any complex algorithmic problems, and instead we can try
+* implementing basic matrix operations,
 
-The Matrix class implementation respects the <a href="http://eigen.tuxfamily.org/index.php?title=Main_Page">Eigen</a> library.
+* deriving equations for tranining simple neural networks, or in other words, computing partial derivatives by using a pen, and
 
-Detailed instruction will come soon!
+* implementing a typical training procedure in a naive manner.
+
+In practice, it would be great to use very useful frameworks like Chainer and Tensorflow!
+
+Note: The Matrix class implementation (e.g., the function names) respects the <a href="http://eigen.tuxfamily.org/index.php?title=Main_Page">Eigen</a> library.
+
+Detailed instruction will come soon.
--- Test.hpp
+++ Test.hpp
@@ -49,28 +49,82 @@ void test1(){
   G.print();
   std::cout << std::endl;
 
-  std::ostringstream oss;
-  oss << "A:" << A.print_() << "\t";
-  oss << "B:" << B.print_() << "\t";
-  oss << "C:" << C.print_() << "\t";
-  oss << "D:" << D.print_() << "\t";
-  oss << "E:" << E.print_() << "\t";
-  oss << "F:" << F.print_() << "\t";
-  oss << "G:" << G.print_() << "\t";
+  /*  
+  std::ofstream a("./test/TEST1_A.txt");
+  a << A.print_() << std::endl;
+  std::ofstream b("./test/TEST1_B.txt");
+  b << B.print_() << std::endl;
+  std::ofstream c("./test/TEST1_C.txt");
+  c << C.print_() << std::endl;
+  std::ofstream d("./test/TEST1_D.txt");
+  d << D.print_() << std::endl;
+  std::ofstream e("./test/TEST1_E.txt");
+  e << E.print_() << std::endl;
+  std::ofstream f("./test/TEST1_F.txt");
+  f << F.print_() << std::endl;
+  std::ofstream g("./test/TEST1_G.txt");
+  g << G.print_() << std::endl;
+  */
   
-  //std::ofstream ofs("./test/TEST1.txt");
-  //ofs << oss.str() << std::endl;
-
-  std::ifstream ifs("./test/TEST1.txt");
+  std::ifstream a("./test/TEST1_A.txt");
+  std::ifstream b("./test/TEST1_B.txt");
+  std::ifstream c("./test/TEST1_C.txt");
+  std::ifstream d("./test/TEST1_D.txt");
+  std::ifstream e("./test/TEST1_E.txt");
+  std::ifstream f("./test/TEST1_F.txt");
+  std::ifstream g("./test/TEST1_G.txt");
   std::string line;
-  std::getline(ifs, line);
 
-  if (line == oss.str()){
-    std::cout << "***** Passed the first test *****" << std::endl;
+  std::getline(a, line);
+  if (line != A.print_()){
+    std::cout << "Something is wrong with A!" << std::endl;
+    std::cout << "Please check the coeff functions." << std::endl;
+    exit(1);
   }
-  else {
-    std::cout << "!!!!! Failed to pass the first test !!!!!" << std::endl;
+
+  std::getline(b, line);
+  if (line != B.print_()){
+    std::cout << "Something is wrong with B!" << std::endl;
+    std::cout << "Please check the coeff functions." << std::endl;
+    exit(1);
   }
+
+  std::getline(c, line);
+  if (line != C.print_()){
+    std::cout << "Something is wrong with C!" << std::endl;
+    std::cout << "Please check the coeff functions." << std::endl;
+    exit(1);
+  }
+
+  std::getline(d, line);
+  if (line != D.print_()){
+    std::cout << "Something is wrong with D!" << std::endl;
+    std::cout << "Please check the matrix-matrix multiplication or addition function." << std::endl;
+    exit(1);
+  }
+
+  std::getline(e, line);
+  if (line != E.print_()){
+    std::cout << "Something is wrong with E!" << std::endl;
+    std::cout << "Please check the transpose function." << std::endl;
+    exit(1);
+  }
+
+  std::getline(f, line);
+  if (line != F.print_()){
+    std::cout << "Something is wrong with F!" << std::endl;
+    std::cout << "Please check the dot function." << std::endl;
+    exit(1);
+  }
+
+  std::getline(g, line);
+  if (line != G.print_()){
+    std::cout << "Something is wrong with G!" << std::endl;
+    std::cout << "Please check the softmax function." << std::endl;
+    exit(1);
+  }
+  
+  std::cout << "***** Passed the first test *****" << std::endl;
 }
 
 void test2(){
